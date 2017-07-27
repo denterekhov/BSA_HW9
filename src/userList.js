5b183011e39ad2b4ce89d9fcfe8bafa77bbb86f5
Denis Terekhov <den.terekhov@ukr.net>
2017-07-27 19:28:53 +0300

    minor fixes

--- src/userList.js
+++ src/userList.js
@@ -3,6 +3,15 @@ import UserItem from './userItem';
 
 
 class UserList extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+      if (nextProps.users.length !== this.props.users.length) {
+        console.log("Yeah! Let's update our userlist!");
+        return true;
+      } else {
+        console.log("Nothing to update :(");
+        return false;
+      }
+  }
   render() {
     const userList = this.props.users.map(obj => 
       <UserItem key={obj.id} user={obj.user} userIndex={obj.id} deleteUser={this.props.deleteUser} />
